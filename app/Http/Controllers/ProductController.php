cb6cd426f9f8646a37cc31148ad51540488b7ab4
enriquesousa <enrique.sousa@gmail.com>
2021-12-17 07:43:42 -0800

    26. Interactuando con la base de datos con el Query Builder de Laravel

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
 
     public function index(){
+        $products = DB::table('products')->get();
+        dd($products);
         return view('products.index');
     }
 
@@ -20,6 +23,8 @@ class ProductController extends Controller
     }
 
     public function show($product){
+        $product = DB::table('products')->where('id', $product)->first();
+        dd($product);
         return view('products.show');
     }
 
