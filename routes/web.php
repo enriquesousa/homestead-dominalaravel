6f06e81967bcd4b99ba0c776db7bc32287a96ccc
enriquesousa <enrique.sousa@gmail.com>
2021-12-29 19:23:50 -0800

    48. Mejorando las rutas usando rutas de recurso en Laravel

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// })->name('main');
-
-// Resolviendo la pagina raiz con un controlador
+// Resolviendo la pagina raíz con un controlador
 Route::get('/', 'MainController@index')->name('main');
 
-// Rutas CRUD products
-Route::get('products', 'ProductController@index')->name('products.index');
-Route::get('products/create', 'ProductController@create')->name('products.create');
-Route::post('products', 'ProductController@store')->name('products.store');
-Route::get('products/{product}', 'ProductController@show')->name('products.show');
-// Route::get('products/{product:title}', 'ProductController@show')->name('products.show');
-Route::get('products/{product}/edit', 'ProductController@edit')->name('products.edit');
-Route::match(['put', 'patch'], 'products/{product}', 'ProductController@update')->name('products.update');
-Route::delete('products/{product}','ProductController@destroy')->name('products.destroy');
-
-
+// Rutas CRUD products en una sola linea
+Route::resource('products', 'ProductController');
 
 Auth::routes();
 
