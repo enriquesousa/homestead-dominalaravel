8103ab1f9678b0000b1831307abbcc631c25f738
enriquesousa <enrique.sousa@gmail.com>
2021-12-31 06:55:41 -0800

    53. Implementando los modelos adicionales de tu proyecto

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'amount',
+        'payed_at',
+    ];
+    // usar atributo $dates para poder usar este campo por Carbon
+    protected $dates = [
+        'payed_at',
+    ];
+
 }
